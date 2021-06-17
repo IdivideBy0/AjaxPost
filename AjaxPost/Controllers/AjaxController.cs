6de425abec65b94124b7732972c3d1ef3aee05f7
Michael Murray <v823061@global.ual.com>
2021-06-17 13:39:26 -0500

    Add project files.

---
+++ AjaxPost/Controllers/AjaxController.cs
@@ -0,0 +1,28 @@
+﻿using AjaxPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxPost.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult PostJson(ParamModel pModel)
+        {
+             string param1 = pModel.param1;
+             string param2 = pModel.param2;
+
+            
+            return Json(param1 + " " + param2, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxPost/Models/ParamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxPost.Models
+{
+    public class ParamModel
+    {
+        public string param1 { get; set; }
+        public string param2 { get; set; }
+    }
+}
